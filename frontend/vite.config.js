25a0ba7af924b7820b152ce07681bf27c0676cdf
adityamatAI <aditya265matai@gmail.com>
2025-07-08 11:42:28 +0400

    clearing

--- frontend/vite.config.js
+++
@@ -1,9 +0,0 @@
-// frontend/vite.config.js
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react' // <--- IMPORT THE PLUGIN
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()], // <--- USE THE PLUGIN
-})
